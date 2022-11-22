854a4f744594d56304b7de83fb2317e20a74e5dd
Gonso <gonsoman1600@gmail.com>
2022-11-22 12:05:11 +0100

    Inclusion comentarios funciones fichas

--- src/Fichas.java
+++ src/Fichas.java
@@ -1,10 +1,17 @@
 public class Fichas {
     private String color;
 
+    /**
+     * @return color ficha actual
+     */
     public String getColor(){
         return color;
     }
 
+    /**
+     * Establece color de ficha
+     * @param color ficha
+     */
     public void setColor(String color) {
         this.color = color;
     }
