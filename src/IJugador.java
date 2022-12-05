9f0ad3a52d6ac1f6d96a3b1d3d6b985a08d07347
Albertoib01 <albertoiglesias144@gmail.com>
2022-12-05 18:34:01 +0100

    Clase jugador

---
+++ src/IJugador.java
@@ -0,0 +1,15 @@
+public interface IJugador {
+
+    /**
+     * Establece valor de la ficha del jugador
+     * @param color valor del caracter
+     */
+    void setColor(String color);
+
+
+    /**
+     * Proporciona el valor de la ficha pedida
+     * @return ficha del jugador indicado
+     */
+    String getColor();
+}
