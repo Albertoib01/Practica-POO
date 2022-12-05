6bacc13a2251cd7138b1810a3860985dfd247626
Albertoib01 <albertoiglesias144@gmail.com>
2022-12-05 18:09:28 +0100

    Clase menu y jugador introducidas

---
+++ src/Jugador.java
@@ -0,0 +1,18 @@
+public class Jugador implements IJugador{
+
+
+    private char ficha;
+
+    public Jugador() {
+
+    }
+
+    public void setFicha(char x){
+        this.ficha = x;
+    }
+
+    public char getFicha() {
+        return ficha;
+    }
+
+}
---
+++ src/Menu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public Menu(){
+        Partida conecta4 = new Partida("R","Y");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        System.out.println("---------Conecta 4---------\n");
+        System.out.println("1.Jugar\n2.Salir");
+        opcion = sc.nextInt();
+        switch (opcion){
+            case 0: break;
+            case 1: conecta4.Comenzar(); break;
+        }
+    }
+}
