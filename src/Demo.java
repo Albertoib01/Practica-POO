83e593e3b32e2fdab23cd5876ccac8a9c55eb6dd
Albertoib01 <albertoiglesias144@gmail.com>
2022-12-14 11:48:42 +0100

    Clases útiles

--- src/Demo.java
+++ src/Demo.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class Demo implements IJuego{
 
 
@@ -62,3 +63,55 @@ public class Demo implements IJuego{
 
     }
 }
+=======
+public class Demo {
+
+
+        static int PosColumn;
+        static Conecta4 tablero;
+        static MiniMax cpu;
+
+        public void ComenzarDemo() {
+
+            cpu = new MiniMax(Conecta4.Y);
+            cpu = new MiniMax(Conecta4.R);
+            tablero = new Conecta4();
+
+            tablero.printTablero();
+
+            while(!tablero.Final()) {
+                System.out.println();
+                switch (tablero.ultimoJugador) {
+
+                    case Conecta4.Y:
+                        Tabla computerMove1 = cpu.getProxDesplaza(tablero);
+                        tablero.desplazaColumna(computerMove1.col, Conecta4.R);
+                        System.out.println("Turn: CPU R\nInsertada en columna: "+(computerMove1.col+1)+".");
+                        System.out.println();
+                        break;
+
+                    case Conecta4.R:
+                        Tabla computerMove = cpu.getProxDesplaza(tablero);
+                        tablero.desplazaColumna(computerMove.col, Conecta4.Y);
+                        System.out.println("Turn: CPU Y\nInsertada en columna: "+(computerMove.col+1));
+                        break;
+                    default:
+                        break;
+                }
+                tablero.printTablero();
+            }
+
+            System.out.println();
+            if (tablero.ganadorAux == Conecta4.R) {
+                System.out.println("CPU R GANA!");
+                System.out.println(tablero.Ganador);
+            } else if (tablero.ganadorAux == Conecta4.Y) {
+                System.out.println("CPU Y GANA!");
+                System.out.println(tablero.Ganador);
+            } else {
+                System.out.println("TIED!!");
+            }
+        }
+    }
+
+>>>>>>> 4be5dc2 (Clases útiles)
--- src/Entrenamiento.java
+++ src/Entrenamiento.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Entrenamiento implements IJuego{
@@ -65,3 +66,65 @@ public class Entrenamiento implements IJuego{
     }
 
 }
+=======
+import java.util.Scanner;
+
+public class Entrenamiento {
+        static int PosColumn;
+        static Conecta4 tablero;
+        //se define el usuario cpu
+        static MiniMax cpu;
+
+        public void entrenamiento() {
+
+            cpu = new MiniMax(Conecta4.Y);
+            tablero = new Conecta4();
+            tablero.printTablero();
+
+            while(!tablero.Final()) {
+                System.out.println();
+                switch (tablero.ultimoJugador) {
+
+                    case Conecta4.Y:
+                        System.out.print("Turn: R");
+                        try {
+                            do {
+                                System.out.print("\nEnter a column to drop a token (1-7): ");
+                                Scanner in = new Scanner(System.in);
+                                PosColumn = in.nextInt();
+                            } while (tablero.ColumnaLlena(PosColumn -1));
+                        } catch (Exception e){
+                            System.out.println("Numero invalido. Values [1-7]");
+                            break;
+                        }
+
+                        tablero.desplazaColumna(PosColumn -1, Conecta4.R);
+                        System.out.println();
+                        break;
+
+                    case Conecta4.R:
+                        Tabla computerMove = cpu.getProxDesplaza(tablero);
+                        tablero.desplazaColumna(computerMove.col, Conecta4.Y);
+                        System.out.println("Turn: CPU Y\nInsertada en columna: "+(computerMove.col+1));
+                        break;
+                    default:
+                        break;
+                }
+                tablero.printTablero();
+            }
+
+            System.out.println();
+            if (tablero.ganadorAux == Conecta4.R) {
+                System.out.println("R Gana!");
+                System.out.println(tablero.Ganador);
+            } else if (tablero.ganadorAux == Conecta4.Y) {
+                System.out.println("CPU Y GANA!");
+                System.out.println(tablero.Ganador);
+            } else {
+                System.out.println("TIED!!");
+            }
+
+        }
+
+}
+>>>>>>> 4be5dc2 (Clases útiles)
