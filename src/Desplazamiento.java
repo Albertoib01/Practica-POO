7ff5f66a9761f7b93bc55ad01cba350aae66990b
Albertoib01 <albertoiglesias144@gmail.com>
2022-12-16 11:24:04 +0100

    Desplazamiento

---
+++ src/Desplazamiento.java
@@ -0,0 +1,50 @@
+public class Desplazamiento {
+
+    public Desplazamiento(){
+
+    }
+
+    private Tabla tabla = new Tabla();
+
+    /**
+     *
+     * @param row numero de fila
+     * @param col numero de columna
+     * @return tablero
+     */
+    public Tabla desplazamiento(int row, int col) {
+        Tabla desplazamientoElegido = new Tabla();
+        desplazamientoElegido.fila = row;
+        desplazamientoElegido.col = col;
+        desplazamientoElegido.setValor(-1);
+        return desplazamientoElegido;
+    }
+
+    /**
+     *
+     * @param row numero fila
+     * @param col numero columna
+     * @param value
+     * @return tablero
+     */
+    public Tabla desplazamientoPosible(int row, int col, int value) {
+        Tabla desplazaAux = new Tabla();
+        desplazaAux.fila = row;
+        desplazaAux.col = col;
+        desplazaAux.setValor(value);
+        return desplazaAux;
+    }
+
+    /**
+     *
+     * @param value
+     * @return tablero
+     */
+    public Tabla comparaDesplaza(int value) {
+        Tabla comparaAux = new Tabla();
+        comparaAux.fila = -1;
+        comparaAux.col = -1;
+        comparaAux.setValor(value);
+        return comparaAux;
+    }
+}
